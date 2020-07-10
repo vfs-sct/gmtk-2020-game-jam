c3e3118ec3d7617dd67f47cfb12b31faed587657
Diego <diego.castagne@hotmail.com>
2020-07-10 16:03:33 -0700

    added: basic input

--- Assets/Scripts/Gameplay/Character/Player/InputController.cs
+++ Assets/Scripts/Gameplay/Character/Player/InputController.cs
@@ -8,12 +8,27 @@ namespace Afloat
     public class InputController : MonoBehaviour
     {
         // ## UNITY EDITOR ##
+        [SerializeField] private string _xMoveInput = "";
+        [SerializeField] private string _yMoveInput = "";
+        [SerializeField] private string _xAimInput = "";
+        [SerializeField] private string _yAimInput = "";
+
         // ## PROPERTIES  ##
         // ## PUBLIC VARS ##
         // ## PROTECTED VARS ##
         // ## PRIVATE UTIL VARS ##
+        public Vector2 _moveInput;
+        public Vector2 _aimInput;
 
 #region // ## MONOBEHAVIOUR METHODS ##
+
+        private void Update()
+        {
+            _moveInput = new Vector2(Input.GetAxis(_xMoveInput), Input.GetAxis(_yMoveInput));
+
+            _aimInput = new Vector2(Input.GetAxis(_xAimInput), Input.GetAxis(_yAimInput));
+        }
+
 #endregion
 
 #region // ## PUBLIC METHODS ##
