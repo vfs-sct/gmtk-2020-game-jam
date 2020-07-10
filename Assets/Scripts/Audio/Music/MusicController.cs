ae9cbc1d2a4ef422457921652cc5a1744b6e1aba
Sabastian <sabastianp@hotmail.ca>
2020-07-10 15:48:53 -0700

    added: simple music controller

---
+++ Assets/Scripts/Audio/Music/MusicController.cs
@@ -0,0 +1,73 @@
+﻿
+/*
+    Copyright (C) 2020 Team Triple Double, Sabastian Peters
+*/
+
+using System.Collections;
+using UnityEngine;
+
+namespace Afloat
+{
+    /*
+        Class: MusicController
+    */
+    public class MusicController : MonoBehaviour
+    {
+        
+        // ## UNITY EDITOR ##
+
+        [SerializeField] private TrackData _track;
+        
+        
+        // ## PROPERTIES  ##
+        
+        
+        // ## PUBLIC VARS ##
+        
+        
+        // ## PROTECTED VARS ##
+        
+        
+        // ## PRIVATE UTIL VARS ##
+        
+        
+        
+              
+#region // ## MONOBEHAVIOUR METHODS ##
+                
+        IEnumerator Start ()
+        {
+            yield return _track.CallOnBeat(OnBeat);
+        }
+        
+#endregion      
+
+#region // ## PUBLIC METHODS ##
+                
+        
+        
+#endregion       
+
+#region // ## <SOME INTERFACE> METHODS ##   
+                
+        
+        
+#endregion
+        
+#region // ## PROTECTED METHODS ##   
+        
+        
+        
+#endregion
+        
+#region // ## PRIVATE METHODS ##   
+        
+        private void OnBeat ()
+        {
+            UnityEditor.EditorApplication.Beep();
+        }
+        
+#endregion
+
+    }
+}
