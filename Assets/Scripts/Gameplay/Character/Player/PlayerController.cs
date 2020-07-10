2ec05c96b6e32c75c73313a415b7fdd3a853104b
Diego <diego.castagne@hotmail.com>
2020-07-10 15:35:55 -0700

    added: readded diego sandbox scene and some basic player setup

---
+++ Assets/Scripts/Gameplay/Character/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿/*
+    Copyright (C) 2020 Team Triple Double, Diego Castagne
+*/
+using UnityEngine;
+
+namespace Afloat
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // ## UNITY EDITOR ##
+        // ## PROPERTIES  ##
+        // ## PUBLIC VARS ##
+        // ## PROTECTED VARS ##
+        // ## PRIVATE UTIL VARS ##
+
+#region // ## MONOBEHAVIOUR METHODS ##
+#endregion
+
+#region // ## PUBLIC METHODS ##
+#endregion
+        
+#region // ## <SOME INTERFACE> METHODS ##   
+#endregion
+
+#region // ## PROTECTED METHODS ##   
+#endregion
+        
+#region // ## PRIVATE METHODS ##   
+#endregion
+
+    }
+}
