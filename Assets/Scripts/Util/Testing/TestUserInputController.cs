1db8f91809b8649382629dffffd06753106896c3
Sabastian <sabastianp@hotmail.ca>
2020-07-10 10:52:30 -0700

    added: test user input

---
+++ Assets/Scripts/Util/Testing/TestUserInputController.cs
@@ -0,0 +1,47 @@
+﻿
+/*
+    Copyright (C) 2020 Team Triple Double, Sabastian Peters
+
+    Gets input from the User for doing mechanic tests.
+*/
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Afloat.Util.Testing
+{
+    public class TestUserInputController : MonoBehaviour
+    {
+
+        // ## UNITY EDITOR ##
+        
+        [SerializeField] private List<TestUserInputKey> _userInputKeyList;
+
+
+              
+#region // ## MONOBEHAVIOUR METHODS ##
+        
+        private void Update()
+        {
+            // triggers key down, held & up events for each provided key
+            foreach (var key in _userInputKeyList)
+            {
+                if(Input.GetKeyDown(key.KeyCode))
+                {
+                    key.OnKeyDown.Invoke();
+                }
+                if(Input.GetKey(key.KeyCode))
+                {
+                    key.OnKeyHeld.Invoke();
+                }
+                if(Input.GetKeyUp(key.KeyCode))
+                {
+                    key.OnKeyUp.Invoke();
+                }
+            }
+        }
+        
+#endregion      
+
+    }
+}
---
+++ Assets/Scripts/Util/Testing/TestUserInputKey.cs
@@ -0,0 +1,21 @@
+﻿
+/*
+    Copyright (C) 2020 Team Triple Double, Sabastian Peters
+
+    Data for detecting a single input from the User
+*/
+
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Afloat.Util.Testing
+{
+    [System.Serializable]
+    public class TestUserInputKey 
+    {
+        public KeyCode KeyCode;
+        public UnityEvent OnKeyDown;
+        public UnityEvent OnKeyHeld;
+        public UnityEvent OnKeyUp;
+    }
+}
