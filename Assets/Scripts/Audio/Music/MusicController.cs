1c036dda865681cdd8eda7cd557ded93ce92dcc2
Sabastian <sabastianp@hotmail.ca>
2020-07-11 22:22:05 -0700

    added: music transitions

--- Assets/Scripts/Audio/Music/MusicController.cs
+++ Assets/Scripts/Audio/Music/MusicController.cs
@@ -4,11 +4,9 @@
 */
 
 using System.Collections;
-using System.IO;
-using System.Linq;
 using Afloat.Events;
-using Melanchall.DryWetMidi.Core;
-using Melanchall.DryWetMidi.Interaction;
+using Afloat.Util.Coroutines;
+using GameSavvy.OpenUnityAttributes;
 using UnityEngine;
 
 namespace Afloat
@@ -22,15 +20,34 @@ namespace Afloat
         // ## UNITY EDITOR ##
 
         [Header("References")]
-        [SerializeField] private AudioSource _target;
-        [SerializeField] private TrackData _track;
+        [SerializeField] private TrackData _startingTrack = null;
+
+        [Header("Lines")]
+        [SerializeField] private AudioSource _line1 = null;
+        [SerializeField] private AudioSource _line2 = null;
+        [SerializeField] private AudioSource _transitionLine = null;
+
+        [Header("Song Transition")]
+        [SerializeField] private AudioClip _trackStart = null;
+        [SerializeField] private AudioClip _trackStop = null;
 
         [Header("On Beat")]
-        [SerializeField] private AudioClip _actionClip;
-        [SerializeField] private GameEvent _shootEvent;
+        [SerializeField] private AudioClip _actionClip = null;
+        [SerializeField] private GameEvent _shootEvent = null;
         
         
         // ## PROPERTIES  ##
+
+        private AudioSource FreeLine
+        {
+            get
+            {
+                if(_lastLine == _line1) return _lastLine = _line2;
+                if(_lastLine == _line2) return _lastLine = _line1;
+                
+                return _lastLine = _line1; /// when null
+            }
+        }
         
         
         // ## PUBLIC VARS ##
@@ -41,6 +58,9 @@ namespace Afloat
         
         // ## PRIVATE UTIL VARS ##
         
+        private TrackData _currentTrack = null;
+        private AudioSource _lastLine = null;
+        private CoroutineHandler _playRoutine = null;
         
         
               
@@ -48,15 +68,28 @@ namespace Afloat
                 
         void Start ()
         {
-            _track.Play(this, _target, OnBeat);
+            _playRoutine = new CoroutineHandler(this);
+
+            if(_startingTrack != null)
+            {
+                PlayTrack(_startingTrack);
+            }
         }
         
 #endregion      
 
 #region // ## PUBLIC METHODS ##
-                
+
+        public void PlayTrack (TrackData track)
+        {
+            _playRoutine.Start(PlayRoutine(track));
+        }
         
-        
+        public void Stop ()
+        {
+            _playRoutine.Start(StopRoutine());
+        }
+
 #endregion       
 
 #region // ## <SOME INTERFACE> METHODS ##   
@@ -72,6 +105,35 @@ namespace Afloat
 #endregion
         
 #region // ## PRIVATE METHODS ##   
+
+        private IEnumerator PlayRoutine (TrackData track)
+        {
+            yield return StopRoutine(); /// stops last track
+
+            // plays and waits for transition clip
+            _transitionLine.clip = _trackStart;
+            _transitionLine.Play();
+            yield return new WaitForSeconds(_trackStart.length);
+
+            // plays new track
+            _currentTrack = track;
+            _currentTrack.Play(this, FreeLine, OnBeat);
+        }
+
+        private IEnumerator StopRoutine ()
+        {
+            if(_currentTrack != null)
+            {
+                // stops current track if exists
+                _currentTrack.Stop();
+                _currentTrack = null;
+
+                // plays and waits for transition clip
+                _transitionLine.clip = _trackStop;
+                _transitionLine.Play();
+                yield return new WaitForSeconds(_trackStop.length);
+            }
+        }
         
         private void OnBeat ()
         {
